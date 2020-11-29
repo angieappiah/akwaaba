4e4215dc58aa81e483e5abbcce855e1b976b7204
angieappiah <alegnalawn2@yahoo.com>
2020-11-29 11:55:39 -0800

    backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :photos
-  resources :sites
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/test', to: 'application#test'
+  namespace :api do
+    namespace :v1 do
+     resources :photos
+     resources :sites
+      # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+     get '/test', to: 'application#test'
+    end
+  end
 end
