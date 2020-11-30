0c6e73b66809bad36e86b91ed96ce4ed5ef640bb
angieappiah <alegnalawn2@yahoo.com>
2020-11-29 21:40:07 -0800

    frontend

---
+++ akwaaba/frontend/src/adapters/SitesAdapter.js
@@ -0,0 +1,9 @@
+class SiteAdapter {
+    constructor(){
+        this.baseUrl = 'http://localhost:3000/api/v1/sites'
+    }
+
+    getSites(){
+        return fetch(this.baseUrl).then(res => res.json())
+    } 
+}
